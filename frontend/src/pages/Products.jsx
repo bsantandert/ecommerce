e82a12cf96b68acf5e663abdbfd0b08de846516f
Brandon Santander <brandon.santander.terceros@gmail.com>
2023-04-03 03:05:49 -0400

    move product card to its own component

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,47 +1,10 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { fetchData } from "../api/fetchAPI";
-import {
-  Card,
-  Image,
-  Text,
-  Group,
-  Button,
-  SimpleGrid,
-  createStyles,
-  rem,
-} from "@mantine/core";
-
-const useStyles = createStyles((theme) => ({
-  card: {
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-  },
-
-  section: {
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
-    }`,
-    paddingLeft: theme.spacing.md,
-    paddingRight: theme.spacing.md,
-    paddingBottom: theme.spacing.md,
-  },
-
-  like: {
-    color: theme.colors.red[6],
-  },
-
-  label: {
-    textTransform: "uppercase",
-    fontSize: theme.fontSizes.xs,
-    fontWeight: 700,
-  },
-}));
+import { SimpleGrid } from "@mantine/core";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const navigate = useNavigate();
-  const { classes, theme } = useStyles();
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -51,38 +14,10 @@ const Products = () => {
     fetchProducts();
   }, []);
 
-  const goToProductDetail = (path) => {
-    navigate(path);
-  };
-
   return (
     <SimpleGrid cols={4} spacing="lg" verticalSpacing="xl">
       {products.map((product, index) => (
-        <Card key={index} withBorder radius="md" p="md" className={classes.card}>
-          <Card.Section>
-            <Image src={product.image_url} alt={product.name} height={450} />
-          </Card.Section>
-          <Card.Section className={classes.section} mt="md">
-            <Group position="apart">
-              <Text fz="lg" fw={500}>
-                {product.name}
-              </Text>
-            </Group>
-            <Text fw={700}>{product.price} $</Text>
-            <Text fz="sm" mt="xs">
-              {product.description}
-            </Text>
-          </Card.Section>
-          <Group mt="xs">
-            <Button
-              radius="md"
-              style={{ flex: 1 }}
-              onClick={() => goToProductDetail(`/products/${product.id}`)}
-            >
-              Show details
-            </Button>
-          </Group>
-        </Card>
+        <ProductCard key={index} product={product}></ProductCard>
       ))}
     </SimpleGrid>
   );
