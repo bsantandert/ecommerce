0b49b50526deaf2d5b026bc8bd464ae9c29944dd
Brandon Santander <brandon.santander.terceros@gmail.com>
2023-04-03 02:06:01 -0400

    add cors to service

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+const cors = require("cors");
 
-var productsRouter = require('./routes/products.route');
-var ordersRouter = require('./routes/orders.route');
+var productsRouter = require("./routes/products.route");
+var ordersRouter = require("./routes/orders.route");
 
 var app = express();
 
@@ -12,13 +13,18 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", function (req, res) {
   res.send("Backend Service");
 });
 
-app.use('/products', productsRouter);
-app.use('/orders', ordersRouter);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
