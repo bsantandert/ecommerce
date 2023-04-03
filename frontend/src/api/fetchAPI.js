e8c47786932375a42181373c886c55a900a15ac7
Brandon Santander <brandon.santander.terceros@gmail.com>
2023-04-03 02:06:27 -0400

    add fetchAPI to request data to service

---
+++ frontend/src/api/fetchAPI.js
@@ -0,0 +1,18 @@
+const fetchJSON = async (url, options = {}) => {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    const error = await response.json();
+    throw error;
+  }
+  if (response.status === 204) {
+    return {};
+  }
+  return await response.json();
+};
+
+const fetchData = async (appendUrl, options) => {
+  const url = `${process.env.REACT_APP_BACKEND_URL}/${appendUrl}`;
+  return await fetchJSON(url, options);
+};
+
+export { fetchJSON, fetchData };
