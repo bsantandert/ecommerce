6c080e133c4ea1f398ca0f6ccbd28eabcb0b9e3e
Brandon Santander <brandon.santander.terceros@gmail.com>
2023-04-05 03:40:48 -0400

    add search in products page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,21 +1,40 @@
 import { useState, useEffect } from "react";
 import { fetchData } from "../api/fetchAPI";
-import { SimpleGrid, Container } from "@mantine/core";
+import { SimpleGrid, Container, Autocomplete } from "@mantine/core";
 import ProductCard from "../components/ProductCard";
+import { IconSearch } from "@tabler/icons-react";
+import { useSearchParams } from "react-router-dom";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productOptions = products.map((p) => p.name);
 
   useEffect(() => {
+    const searchString = searchParams.get("search");
     const fetchProducts = async () => {
-      const products = await fetchData("products");
+      const products = await fetchData(
+        searchString ? `products?search=${searchString}` : "products"
+      );
       setProducts(products.data);
     };
     fetchProducts();
-  }, []);
+  }, [searchParams]);
+
+  const search = (value) => {
+    setSearchParams(`search=${value}`);
+  };
 
   return (
     <Container>
+      <Autocomplete
+        label="Search"
+        placeholder="Search Product"
+        rightSection={<IconSearch size="1rem" />}
+        onChange={search}
+        data={productOptions}
+      />
+
       <SimpleGrid cols={3} spacing="lg" verticalSpacing="xl">
         {products.map((product) => (
           <ProductCard product={product}></ProductCard>
